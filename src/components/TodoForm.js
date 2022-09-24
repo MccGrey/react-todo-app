758f39288313c6c9bd659bbef513b15b13214832
MccGrey <chuxzzie01@yahoo.com>
2022-09-24 13:37:14 +0100

    todo app progress

---
+++ src/components/TodoForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export const TodoForm = (props) => {
+  // Usestate
+  const [input, setInput] = useState("");
+  // end of usestate
+
+  // function for submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addTodo(input);
+    setInput("");
+  };
+  // end submit function
+  return (
+    <form onSubmit={handleSubmit} className="todo-form">
+      <input
+        onChange={(e) => setInput(e.target.value)}
+        className="todo-input"
+        placeholder="Add a Todo"
+      />
+      <button type="submit" className="todo-button">
+        Add Todo
+      </button>
+    </form>
+  );
+};
